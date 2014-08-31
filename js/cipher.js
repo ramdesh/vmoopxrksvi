d0ad13bb970770e38ed13fb06c37cd74fbc7f79e
Kulendra <kulendra@gmail.com>
2014-08-31 17:23:46 +0530

    atbash() added
    Function atbash added.

--- js/cipher.js
+++ js/cipher.js
@@ -6,3 +6,33 @@ function reverse() {
 
   outputArea.value = reversedString;
 }
+
+function atbash(text){
+/**
+ * Converts the input text in to an atbash cipher using an alphabet [a-z] and [A-Z]. 
+ * Numbers and all other characters are left as they are.
+ * @param {string} text - text to be atbashed.
+ * @return {string} - returns atbash converted string of the input.
+ * @todo - Currently this function uses a pure a-z alphabet and uses the ASCII offset 
+ * to find the atbash. Will not work with anything outside the a-z alphabet OR numbers.
+ * @author - KJ <kulendra@gmail.com>
+ */
+	var i = 0;
+	var character = "";
+	var outputchar = "";
+	var outputtext = "";
+	for(i=0; i<text.length;i++){
+		character = text.charCodeAt(i);
+		if(character >= 65 && character <= 90){
+			outputchar = fromCharCode(155-character);
+		}
+		elseif(character >= 97 && character <=122){
+			outputchar = String.fromCharCode(219 - character);
+		}
+		else{
+			outputchar = String.fromCharCode(character);
+		}
+		outputtext = outputtext+outputchar;
+	}
+	return outputtext;
+}
