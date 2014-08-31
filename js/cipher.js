ae752f89e981bade319824d5b8a0bf9be5270e19
Kulendra <kulendra@gmail.com>
2014-08-31 17:45:16 +0530

    caesar() added
    Caesar cipher added.

--- js/cipher.js
+++ js/cipher.js
@@ -36,3 +36,31 @@ function atbash(text){
 	}
 	return outputtext;
 }
+
+function caesar(text,n,flag){
+/**
+ * Converts the input text using caesar cipher using an alphabet [a-z] and [A-Z]. 
+ * @param {string} text - text to be encrypted/decrypted.
+ * @param {integer} n - shift for caesar.
+ * @param {integer} flag - 0: for encrypt 1: for decrypt
+ * @return {string} - returns caesar encrypted/decrypted text.
+ */
+	var i = 0;
+	var character = "";
+	var outputchar = "";
+	var outputtext = "";
+	for(i=0; i<text.length;i++){
+		character = text.charCodeAt(i);
+		if(character >= 65 && character <= 90){
+			outputchar = String.fromCharCode(65 + ((character+n)%90));
+		}
+		elseif(character >= 97 && character <=122){
+			outputchar = String.fromCharCode(97 + ((character+n)%122));
+		}
+		else{
+			outputchar = fromCharCode(character);
+		}
+		outputtext = outputtext+outputchar;
+	}
+	return outputtext;
+}
