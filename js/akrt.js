b12ea1aa50109704d8ffda14c5a15d0266db46e5
ramdesh <rasade88@gmail.com>
2014-09-21 00:53:25 +0530

    Made fixes to akrt

--- js/akrt.js
+++ js/akrt.js
@@ -1,10 +1,18 @@
 function replaceCipher(ciphertext, start, end) {
+	var prefix = ciphertext.substring(0,start);
+	var suffix = ciphertext.substring(end);
 	var outputhtml = "";
 	$.getJSON("keywords.json", function(json) {
 		$.each(json.keywords, function(i, keyword) {
+			var outstring = '<div class="col-md-4"><p>' + prefix + keyword + suffix + '</p></div>';
 			if ((i % 3) == 0) {
 				outputhtml += '<div class="row">'
-				outputhtml += '	'
+				outputhtml += outstring;
+			} else if ((i % 3) == 2) {
+				outputhtml += outstring;
+				outputhtml += '</div>';
+			} else {
+				outputhtml += outstring;
 			}
 		});
 	});
