5f7bf287167446a19e99b0ecbf3196fbe6ea8e31
Kulendra <kulendra@gmail.com>
2015-03-08 15:56:23 +0530

    Updated formValidate()
    Corrected line 66. The if condition was checking the wrong condition.

--- js/formhelper.js
+++ js/formhelper.js
@@ -63,7 +63,7 @@ function formValidate(obj,text){
 	}
 	if(obj){
 		$(obj).removeClass("btn-success btn-warning btn-info");
-		if(typeof classstring === 'undefined'){
+		if(typeof classstring !== 'undefined'){
 			$(obj).addClass(classstring);
 		}
 	}
